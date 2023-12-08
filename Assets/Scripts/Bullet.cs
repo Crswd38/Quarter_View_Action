235fa047e5c2c9cd015c9ca9750ea91dee31c02c
Crswd38 <sinsoulward38@gmail.com>
2023-12-08 14:58:22 +0900

    Quarter_View_Action

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public int damage;
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Floor") {
+            Destroy(gameObject, 3f);
+        }
+    }
+
+    void OnTriggerEnter(Collider collision) {
+        if(collision.gameObject.tag == "Wall") {
+            Destroy(gameObject);
+        }else if(collision.gameObject.tag == "Item" || collision.gameObject.tag == "Weapon") {
+            Destroy(gameObject, 3f);
+        }
+    }
+}
